78e9e4d55657b5a41b8e1316caa76971f7060a13
Yami-no-karuro <77389905+Yami-no-karuro@users.noreply.github.com>
2022-10-10 15:43:52 +0200

    20221010

--- src/controllers/ExampleController.php
+++ src/controllers/ExampleController.php
@@ -2,6 +2,7 @@
 namespace Vector\Controllers;
 use Vector\Router;
 use Vector\Objects\Response;
+use Vector\Functions\MySqlConnect;
 
 if (!defined('NO_DIRECT_ACCESS')) { 
     header('HTTP/1.0 403 Forbidden');
@@ -35,31 +36,29 @@ class ExampleController {
         });
 
         $this->router->register_route(['GET'], '^/posts/?$', function(): Response {
-            $postlist = json_encode([
-                [
-                    'ID' => 1,
-                    'Title' => 'Lorem Ipsum',
-                    'Content' => 'Lorem Ipsum Dolor Sit'
-                ],
-                [
-                    'ID' => 2,
-                    'Title' => 'Lorem Ipsum',
-                    'Content' => 'Lorem Ipsum Dolor Sit'
-                ]
-            ]);
-            return new Response($postlist, [
+            $mysql = MySqlConnect::get_instance();
+            if (isset($_GET['search'])) {
+                $posts = $mysql->get_results("SELECT * FROM `wp_posts` WHERE 
+                    `post_title` LIKE ? OR
+                    `post_content` LIKE ?", array(
+                        ['type' => 's', 'value' => '%' . $_GET['search'] . '%'],
+                        ['type' => 's', 'value' => '%' . $_GET['search'] . '%']
+                ));
+            } else { $posts = $mysql->get_results("SELECT * FROM `wp_posts`"); }
+            if (false === $posts['success']) { return new Response(NULL, ['HTTP/1.1 500 Internal Server Error']); }
+            return new Response(json_encode($posts['data']), [
                 'HTTP/1.1 200 OK',
                 'Content-Type: application/json'
             ]);
         });
 
         $this->router->register_route(['GET'], '^/posts/(?<id>\d+)/?$', function($params): Response {
-            $post = json_encode([
-                'ID' => $params['id'],
-                'Title' => 'Lorem Ipsum',
-                'Content' => 'Lorem Ipsum Dolor Sit'
-            ]); 
-            return new Response($post, [
+            $mysql = MySqlConnect::get_instance();
+            $posts = $mysql->get_results("SELECT * FROM `wp_posts` WHERE `ID` = ?", array(
+                ['type' => 'i', 'value' => $params['id']]
+            ));
+            if (false === $posts['success']) { return new Response(NULL, ['HTTP/1.1 500 Internal Server Error']); }
+            return new Response(json_encode($posts['data']), [
                 'HTTP/1.1 200 OK',
                 'Content-Type: application/json'
             ]);
@@ -68,24 +67,6 @@ class ExampleController {
         $this->router->register_route(['POST'], '^/posts/?$', function(): Response {
             $req_body = file_get_contents('php://input');
             return new Response($req_body, [
-                'HTTP/1.1 200 OK',
-                'Content-Type: application/json'
-            ]);
-        });
-
-        $this->router->register_route(['GET'], '^/search-posts/?$', function(): Response {
-            $mysql = MySqlConnect::get_instance();
-            $posts = array();
-            if (isset($_GET['search'])) {
-                $posts = $mysql->get_results("SELECT * FROM `wp_posts` WHERE 
-                    `post_title` LIKE ? OR
-                    `post_content` LIKE ?", array(
-                        ['type' => 's', 'value' => '%' . $_GET['search'] . '%'],
-                        ['type' => 's', 'value' => '%' . $_GET['search'] . '%']
-                ));
-            }
-            if (false === $posts['success']) { return new Response(NULL, ['HTTP/1.1 500 Internal Server Error']); }
-            return new Response(json_encode($posts['data']), [
                 'HTTP/1.1 200 OK',
                 'Content-Type: application/json'
             ]);

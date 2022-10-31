5df5036d493aa0cf060f8454bb61dabd0f43597e
Yami-no-karuro <77389905+Yami-no-karuro@users.noreply.github.com>
2022-10-31 19:20:00 +0100

    Removing useless parts

--- src/autoload_core.php
+++ src/autoload_core.php
@@ -12,9 +12,5 @@ spl_autoload_register(function($class) {
         require_once(__DIR__ . '/functions/' . $classname . '.php');
     } else if (file_exists(__DIR__ . '/objects/' . $classname . '.php')) {
         require_once(__DIR__ . '/objects/' . $classname . '.php');
-    } else if (file_exists(__DIR__ . '/entities/' . $classname . '.php')) {
-        require_once(__DIR__ . '/entities/' . $classname . '.php');
-    } else if (file_exists(__DIR__ . '/controllers/' . $classname . '.php')) {
-        require_once(__DIR__ . '/controllers/' . $classname . '.php');
     }
 });

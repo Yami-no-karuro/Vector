9a9158b61e290f668a823f2cd4655fb301e162b5
Yami-no-karuro <carloborgna98@gmail.com>
2023-04-22 16:08:43 +0200

    Database transients

--- src/modules/Transient.php
+++ src/modules/Transient.php
@@ -1,6 +1,21 @@
 <?php
 
+/*
+MySql
+----
+CREATE TABLE `<dbname>`.`transients` (
+    `ID` INT NOT NULL AUTO_INCREMENT , 
+    `trs_key` VARCHAR(50) NOT NULL , 
+    `trs_value` TEXT NOT NULL , 
+    `trs_ltmtime` INT NOT NULL , 
+    PRIMARY KEY (`ID`)
+) ENGINE = InnoDB; 
+----
+*/
+
 namespace Vector\Module;
+
+use Vector\Module\SqlConnection;
 
 if (!defined('NO_DIRECT_ACCESS')) { 
     header('HTTP/1.1 403 Forbidden');
@@ -9,9 +24,12 @@ if (!defined('NO_DIRECT_ACCESS')) {
 
 class Transient {
 
-    protected string $filepath;
-    public mixed $content;
-    public mixed $lsmTime;
+    protected string $transient;
+    public mixed $content = null;
+    public null|int $lsmTime = null;
+
+    protected null|string $filepath;
+    protected null|SqlConnection $sql;
 
     /**
      * @package Vector
@@ -20,9 +38,23 @@ class Transient {
      */
     public function __construct(string $transient) 
     {
-        $this->filepath = __DIR__ . '/../var/cache/transients/' . md5($transient);
-        $this->content = @file_get_contents($this->filepath, true);
-        $this->lsmTime = @filemtime($this->filepath);
+        $this->transient = $transient;
+        if (true === DATABASE_TRANSIENTS) {
+            $this->sql = SqlConnection::getInstance();
+            $transient = $this->sql->getResults("SELECT `trs_value`, `trs_ltmtime` 
+                FROM `transients` 
+                WHERE `trs_key` = ?", [
+                    ['type' => 's', 'value' => $this->transient]
+            ]);
+            if ($transient['success'] AND !empty($transient['data'])) {
+                $this->content = $transient['data']['trs_value'];
+                $this->lsmTime = $transient['data']['trs_ltmtime'];
+            }
+        } else {
+            $this->filepath = __DIR__ . '/../var/cache/transients/' . md5($transient);
+            $this->content = @file_get_contents($this->filepath, true);
+            $this->lsmTime = @filemtime($this->filepath);
+        }
     }
 
     /**
@@ -33,6 +65,7 @@ class Transient {
      */
     public function isValid(int $seconds): bool
     {
+        if (!$this->lsmTime) { return false; }
         return (time() - $this->lsmTime) > $seconds ? false : true;
     }
 
@@ -43,8 +76,8 @@ class Transient {
      */
     public function getContent(): mixed 
     {
-        $data = unserialize($this->content);
-        return $this->content;
+        if (!$this->content) { return null; }
+        return unserialize($this->content);
     }
 
     /**
@@ -56,7 +89,26 @@ class Transient {
     public function setContent(mixed $data): bool 
     {
         $srlData = serialize($data);
-        return @file_put_contents($this->filepath, $srlData);
+        if (true === DATABASE_TRANSIENTS) {
+            if (!$this->content) {
+                $execResult = $this->sql->exec("INSERT INTO `transients` 
+                    (`ID`, `trs_key`, `trs_value`, `trs_ltmtime`) 
+                    VALUES (NULL, ?, ?, ?)", [
+                        ['type' => 's', 'value' => $this->transient],
+                        ['type' => 's', 'value' => serialize($data)],
+                        ['type' => 's', 'value' => time()]
+                ]);
+            } else {
+                $execResult = $this->sql->exec("UPDATE `transients` 
+                    SET `trs_value` = ?, `trs_ltmtime` = ?
+                    WHERE `trs_key` = ?", [
+                        ['type' => 's', 'value' => serialize($data)],
+                        ['type' => 's', 'value' => time()],
+                        ['type' => 's', 'value' => $this->transient]
+                ]);
+            }
+            return $execResult['success'];
+        } else { return @file_put_contents($this->filepath, $srlData); }
     }
 
     /**
@@ -66,7 +118,12 @@ class Transient {
      */
     public function delete(): bool
     {
-        return @unlink($this->filepath);
+        if (true === DATABASE_TRANSIENTS) {
+            $execResult = $this->sql->exec("DELETE FROM `transients` WHERE `trs_key` = ?", [
+                ['type' => 's', 'value' => $this->transient]
+            ]);
+            return $execResult['success'];
+        } else { return @unlink($this->filepath); }
     }
 
 }

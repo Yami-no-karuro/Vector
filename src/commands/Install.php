ad1e3693382f61ae1e6ff43ed29dee68bbff461a
Yami-no-karuro <carloborgna98@gmail.com>
2023-06-24 10:40:21 +0200

    StopWatch integration

--- src/commands/Install.php
+++ src/commands/Install.php
@@ -7,6 +7,7 @@ use Vector\Module\Console\AbstractCommand;
 use Vector\Module\SqlClient;
 use Vector\Module\ApplicationLogger\FileSystemLogger;
 use Vector\Module\Console\Application;
+use Vector\Module\StopWatch;
 use FilesystemIterator;
 use RecursiveDirectoryIterator;
 use RecursiveIteratorIterator;
@@ -21,6 +22,7 @@ class Install extends AbstractCommand
 {
     protected SqlClient $sql;
     protected FileSystemLogger $logger;
+    protected StopWatch $stopWatch;
 
     /**
      * @package Vector
@@ -32,6 +34,7 @@ class Install extends AbstractCommand
         parent::__construct($args);
         $this->sql = SqlClient::getInstance();
         $this->logger = new FileSystemLogger('command');
+        $this->stopWatch = new StopWatch();
     }
 
     /**
@@ -41,6 +44,7 @@ class Install extends AbstractCommand
      */
     public function execute(): int
     {
+        $this->stopWatch->start();
         $dir = Kernel::getProjectRoot() . 'var/sql/';
         if (file_exists($dir) and is_dir($dir)) {
             $sqlDir = new RecursiveDirectoryIterator($dir, FilesystemIterator::SKIP_DOTS);
@@ -59,6 +63,9 @@ class Install extends AbstractCommand
                 }
             }
         }
+        $this->stopWatch->stop();
+        Application::out('Vector installed succesfully!');
+        Application::out('Executed for: ' . $this->stopWatch->getElapsed());
         return 0;
     }
 

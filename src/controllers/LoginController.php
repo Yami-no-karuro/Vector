551d784ec139746e1a5b84b9ed6ec81b72555a7c
Yami-no-karuro <carloborgna98@gmail.com>
2023-07-03 09:01:09 +0200

    Refactoring

--- src/controllers/LoginController.php
+++ src/controllers/LoginController.php
@@ -6,11 +6,11 @@ use Vector\Router;
 use Vector\Module\Controller\FrontendController;
 use Vector\Module\Transient\SqlTransient;
 use Vector\Module\SqlClient;
+use Vector\Module\Security\AuthToken;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Cookie;
-use Vector\Module\Security\AuthToken;
 
 if (!defined('NO_DIRECT_ACCESS')) {
     header('HTTP/1.1 403 Forbidden');

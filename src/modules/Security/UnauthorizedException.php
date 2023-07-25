23386ff2d4f972dbdce8b9c2e2ba3ed95236ead6
Yami-no-karuro <carloborgna98@gmail.com>
2023-07-25 12:53:30 +0200

    Firewall and Kernel updates

---
+++ src/modules/Security/UnauthorizedException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Vector\Module\Security;
+
+use Exception;
+
+if (!defined('NO_DIRECT_ACCESS')) {
+    header('HTTP/1.1 403 Forbidden');
+    die();
+}
+
+class UnauthorizedException extends Exception
+{
+}

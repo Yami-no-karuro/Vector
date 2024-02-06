fad56ca782227dd4d68e2b51d008e9283b1b0b50
Yami-no-karuro <carloborgna98@gmail.com>
2024-02-06 11:01:08 +0100

    Refactoring

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,10 +6,10 @@ use Vector\Router;
 use Vector\DataObject\Asset;
 use Vector\Module\Controller\FrontendController;
 use Vector\Module\ApplicationLogger\SqlLogger;
+use Vector\Repository\AssetRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\StreamedResponse;
-use Vector\Repository\AssetRepository;
 
 if (!defined('NO_DIRECT_ACCESS')) {
     header('HTTP/1.1 403 Forbidden');

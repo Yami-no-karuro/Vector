063b4c078f9af49d74bbfc2cdb335d4fd3d9a020
Yami-no-karuro <carloborgna98@gmail.com>
2025-03-22 18:37:22 +0100

    Encore + React (functioning)

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
+import App from './app/App.js';
 import './styles/app.scss';
 
 (() => {
   'use-strict';
-
   const app = document.querySelector('div#app');
   const root = createRoot(app);
 
+  root.render(<App />);
 })();
---
+++ assets/app/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="container">
+      <section className="py-5">
+        <h2> Welcome! </h2>
+      </section>
+    </div>
+  );  
+}
+
+export default App;

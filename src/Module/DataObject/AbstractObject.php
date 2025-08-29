72ca9e4a271c0ca6253348355946bdac4ec5cbec
Yami-no-karuro <carloborgna98@gmail.com>
2025-08-29 12:20:31 +0200

    DataObject events

--- src/Module/DataObject/AbstractObject.php
+++ src/Module/DataObject/AbstractObject.php
@@ -3,6 +3,7 @@
 namespace Vector\Module\Dataobject;
 
 use Vector\Module\DataObject\Attribute\Property;
+use Vector\Module\EventDispatcher;
 use Vector\Module\SqlClient;
 use ReflectionObject;
 use ReflectionProperty;
@@ -75,6 +76,7 @@ abstract class AbstractObject
         $reflection = new ReflectionObject($this);
         $props = $reflection->getProperties(ReflectionProperty::IS_PROTECTED);
         
+        EventDispatcher::dispatch('DataObjectListener', 'onPreSave', [&$this, &$props]);
         foreach ($props as $prop) {
             $attributes = $prop->getAttributes(Property::class);
             if (empty($attributes))
@@ -110,25 +112,33 @@ abstract class AbstractObject
         if (property_exists($this, $primaryKey) && $this->$primaryKey === null)
             $this->$primaryKey = $this->sql->lastInsertId();
 
+        EventDispatcher::dispatch('DataObjectListener', 'onPostSave', [&$this]);
         return $this;
     }
     
     /**
      * @package Vector
      * Vector\Module\DataObject\AbstractObject->delete()
-     * @return void
+     * @return AbstractObject
      */
-    public function delete(): void
+    public function delete(): AbstractObject
     {
         $table = $this->tablename;
         $primaryKey = $this->primary;
-        if ($this->$primaryKey === null) 
-            return;
+        if ($this->$primaryKey === null)
+            return $this;
 
+        EventDispatcher::dispatch('DataObjectListener', 'onPreDelete', [&$this]);
         $sql = "DELETE FROM `{$table}` WHERE `{$primaryKey}` = :id";
+        
         $stmt = $this->sql->prepare($sql);
         $stmt->bindValue(':id', $this->$primaryKey);
         $stmt->execute();
+        
+        $this->$primaryKey = null;
+        
+        EventDispatcher::dispatch('DataObjectListener', 'onPostDelete', [&$this]);
+        return $this;
     }
     
     /**
